e0b72dd83e67d0322b4fd6f8db0de19472ebfc2c
Bei Chen Liu <bei.liu@mail.mcgill.ca>
2017-04-09 18:37:20 -0400

    Add back the diagnostic updater and fix some logic

--- src/dcdc_nuc_node.cpp
+++ src/dcdc_nuc_node.cpp
@@ -19,8 +19,8 @@
 
 #include "dcdc_nuc.h"
 #include <ros/ros.h>
-//#include <diagnostic_updater/diagnostic_updater.h>
-//#include <diagnostic_updater/publisher.h>
+#include <diagnostic_updater/diagnostic_updater.h>
+#include <diagnostic_updater/publisher.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/String.h>
@@ -30,7 +30,7 @@
 Dcdc_Nuc * dcdc_nuc;
 Dcdc_Nuc_Data dcdc_data;
 
-/*void update_status(diagnostic_updater::DiagnosticStatusWrapper & stat) {
+void update_status(diagnostic_updater::DiagnosticStatusWrapper & stat) {
   std::string state;
   switch (dcdc_data.state_machine_state) {
       case 1: state = "Low Power"; break;
@@ -59,7 +59,6 @@ Dcdc_Nuc_Data dcdc_data;
   stat.add("Mobo Alive (USB)", dcdc_data.usb_sense);
   stat.add("Mobo Alive (Power)", dcdc_data.mobo_alive_pout);
   stat.add("Ignition Raised", dcdc_data.ignition_raised);
-  stat.add("Ignition Raised", dcdc_data.ignition_raised);
   stat.add("Thump Ouput Enbaled", dcdc_data.thump_output_enabled);
 
   if (dcdc_data.timer_init) {
@@ -70,10 +69,6 @@ Dcdc_Nuc_Data dcdc_data;
     stat.add("Timer Output On", dcdc_data.timer_ignition_to_output_on);
     stat.mergeSummary(diagnostic_msgs::DiagnosticStatus::WARN,
         "Turning on output soon.");
-  }
-
-  if (dcdc_data.timer_ignition_cancel) {
-    stat.add("Timer Ignition Cancel", dcdc_data.timer_ignition_cancel);
   }
 
   if (dcdc_data.timer_output_on_to_mobo_on_pulse) {
@@ -113,7 +108,7 @@ Dcdc_Nuc_Data dcdc_data;
   stat.addf("Firmware Version", "%d.%d", dcdc_data.firmware_version_major,
       dcdc_data.firmware_version_minor);
 }
-*/
+
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "dcdc_psu");
@@ -123,13 +118,12 @@ int main(int argc, char **argv) {
 
   ros::Rate loop_rate(10);
 
-  /*
-   * diagnostic_updater::Updater updater;
+  diagnostic_updater::Updater updater;
   std::string hardware_id;
   nh.param<std::string>("hardware_id", hardware_id, "dcdc_psu");
   updater.setHardwareID(hardware_id);
   updater.add("Status updater", update_status);
-  */
+
   ros::Publisher v_in_pub =
       nh.advertise<std_msgs::Float64>("input_voltage", 1);
 
@@ -197,6 +191,11 @@ int main(int argc, char **argv) {
       ROS_INFO_ONCE("Ignition Cancel timer reached.");
       ROS_INFO_ONCE("Auto-shutdown enabled");
     }
+
+    if (!dcdc_data.ignition_voltage_good && dcdc_data.output_enabled) {
+      ROS_INFO_THROTTLE(60, "Ignition voltage low. Shutdown Soon!");
+    }
+
     unsigned int mobo_off = dcdc_data.timer_ignition_off_to_mobo_off_pulse;
     if (mobo_off > 0) {
       ROS_WARN_THROTTLE(1, "Sending motherboard off signal in %d second%s!",
@@ -212,7 +211,7 @@ int main(int argc, char **argv) {
       ROS_FATAL_THROTTLE(1, "Hard shutdown in %d seconds", hard_off);
     }
 
-    //updater.update();
+    updater.update();
     loop_rate.sleep();
   }
 
